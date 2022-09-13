2f1e5369877dcf17f1b4c4457dec9b4cd047df74
Cacree21 <111894319+Cacree21@users.noreply.github.com>
2022-09-13 14:45:35 -0500

    Initial Commit

---
+++ Lab 3 User and File I 0.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 3 User and File I 0.cpp
+//
+//  Created by Caleb Acree on 9/13/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
